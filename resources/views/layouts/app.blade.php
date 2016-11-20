b98d265ad36b89a2e282cbb118d5524f32c041e8
AlfredolMacias <frediy079@gmail.com>
2016-11-20 15:37:08 -0600

    Creacion del boton Proyectos en el index del usuario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,9 @@
                         <ul class="nav navbar-nav">
                             <li><a href="{{ url('/home') }}">Home</a></li>
                         </ul>
+                        <ul class="nav navbar-nav">
+                            <li><a href="{{ url('/user/proyectos') }}">Proyectos</a></li>
+                        </ul>
                     @endif
                @endif
 
