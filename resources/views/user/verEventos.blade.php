35523c3b8008daa183d1af137989d9407838a1e5
AlfredolMacias <frediy079@gmail.com>
2016-11-23 23:01:15 -0600

    Pagina para ver todos los eventos registrados

---
+++ resources/views/user/verEventos.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<!--<div class="container">
+        <div class="col-md-10 col-md-offset-1">
+           @foreach($eventos as $evento)
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#{{$evento->id}}">{{$evento->nombre}}</a>
+                            </h4>
+                        </div>
+                        <div id="{{$evento->id}}" class="panel-collapse collapse in">
+                            <div class="panel-body">{{$evento->descripcion}}</div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+                
+        </div>
+</div>-->
+
+<div class="container">
+  <h2>Eventos</h2>
+  <div class="panel-group" id="accordion">
+    @foreach($eventos as $evento)
+    <div class="panel panel-default">
+       <div class="panel-heading">
+          <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#{{$evento->id}}">{{$evento->nombre}}</a>
+           </h4>
+       </div>
+       <div id="{{$evento->id}}" class="panel-collapse collapse in">
+         <div class="panel-body">
+            <div>
+            {{$evento->descripcion}}</div>
+            <button type="button" class="btn btn-info" onclick="redirect({{$evento->id}})">Ver Evento</button>
+         </div>
+        </div>
+    </div>
+    @endforeach 
+   </div>
+   
+  </div>
+  
+</div>
+<script>
+
+    function redirect(x){
+        var url = "evento/"+x;
+        window.location.href = url;
+    }
+
+    /* $(document).ready(function(){
+       $('button#{{$evento->nombre}}').click(function(){
+           alert("Hola");
+            $.ajax({
+                url : '/user/proyecto',
+                type: 'POST',
+                dataType : 'text',
+                beforeSend: function (xhr) {                                      //Antes de enviar la peticion AJAX se incluye el csrf_token para validar la sesion.
+                    var token = $('meta[name="csrf_token"]').attr('content');
+
+                    if (token) {
+                        return xhr.setRequestHeader('X-CSRF-TOKEN', token);
+                    }
+                },
+                success:function(response){
+                    alert(response);
+                }
+            });
+        });
+    });
+*/
+</script>
+
+
+
+
+
+
+
+@endsection
