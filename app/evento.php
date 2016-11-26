e377472350919cd59babde819e1d851af59b972c
AlfredolMacias <frediy079@gmail.com>
2016-11-26 14:42:28 -0600

    Creacion del modelo evento

--- app/evento.php
+++ app/evento.php
@@ -4,7 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class evento extends Model
+class Evento extends Model
 {
-    //
+    protected $fillable = [
+        'nombre', 'descripcion', 'lugar', 'inicio_registro', 'fin_registro', 'inicio_evento', 'fin_evento', 'categoria'
+    ];
+
+    public function categorias()
+    {
+        return $this->belongsToMany('App\Categoria','eventos_categorias','evento_id','categoria_id');
+    }
+    
 }
