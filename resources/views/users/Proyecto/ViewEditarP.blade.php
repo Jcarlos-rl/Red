75c30e1122a0b65909ba8319ca39be235ee9cccb
Luis Gerardo Alvarado Fuentes <Luis Gerardo Alvarado Fuentes>
2016-11-26 22:28:47 -0600

    Se actualizo visto de Edicion e incersion a base

--- resources/views/users/Proyecto/ViewEditarP.blade.php
+++ resources/views/users/Proyecto/ViewEditarP.blade.php
@@ -3,24 +3,26 @@
 @section('content')
 @php ($rol = $proyecto->users()->find(Auth::user()->id)->pivot->rol)
 <div class="container">
-    <h1> Titulo del Proyecto </h1>
-    <textarea name="titulo" rows="1" cols="30">{{$proyecto->nombre}}</textarea>
     <div class="row">
-      @if($rol == 'ROLE_LEADER')
-        <div class="col-md-8">
-      @else
         <div class="col-md-10 col-md-offset-1">
-      @endif
             <div class="panel panel-default">
                 <div class="panel-body">
-                  <h3>Descripción</h3>
-                  <textarea name="descripcion" rows="8" cols="80">{{$proyecto->descripcion}}</textarea>
+                  {!! Form::open(['route'=>[ 'user.proyectos.update','{{$id}}'],'method' => 'PUT']) !!}
+                  <div class="form-group">
+                  <h1> Titulo del Proyecto </h1>
+                    {!! Form::text('nombre',$proyecto->nombre, ['class' => 'form-control']) !!}
+                  </div>
+                  <div class="form-group">
+                  <h1> Descripcion </h1>
+                    {!! Form::text('descripcion',$proyecto->descripcion, ['class' => 'form-control']) !!}
+                  </div>
+                  <div class="form-group">
+                    {!! Form::submit('Actualizar', ['class' => 'btn btn-success']) !!}
+                  </div>
+                  {!! Form::close() !!}
                 </div>
             </div>
-        </div>    
-    </div>
-    <div class="form-group">
-      {!! Form::submit('Actualizar', ['class' => 'btn btn-success']) !!}
+        </div>
     </div>
 </div>
 
