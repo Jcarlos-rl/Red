b3e51dc2c326978effbc24738633cb3d324d45be
Jcarlos-rl <carlos_-619@hotmail.com>
2016-11-29 17:30:32 -0600

    Modificando estilos para cargar imagen de perfil

--- resources/views/users/configuracionCuenta.blade.php
+++ resources/views/users/configuracionCuenta.blade.php
@@ -20,25 +20,39 @@
                             </div>
                         </div>
                     </form>
-                    <div class="col-md-8">
+                    <div class="col-md-12">
                         <form class="form-horizaontal" method="POST" action="/user/subirImagenPerfil" enctype="multipart/form-data">
                         {{ csrf_field() }}
                             <div class="form-group">
-                                <label class="col-sm-2 control-label">Foto Perfil</label>
-                                <div class="col-sm-10">
-                                <input type="file" class="form-control" name="imagen">
+                                <label class="perfil col-sm-3 control-label">Foto Perfil</label>
+                                <div class="col-sm-9">
+                                    <input type="file" class="form-control" name="imagen">
                                 </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <br>
+                            </div>
+                            <div class="col-md-12">
+                                <center><img src="{{ asset( 'imagenesPerfil/' . $user->imagenPerfil)}}" class="img-responsive" style="object-fit:cover; width:100%; max-width:150px;"></center>
+                            </div>
+                            <div class="col-sm-12">
+                                <br>
                             </div>
                             <div class="form-group">
                                 <div class="col-sm-10 col-sm-offset-2">
-                                    <button type="submit" class="btn btn-default form-control" >Cambiar Imagen</button>
+                                    <button type="submit" class="btn btn-primary form-control" >Cambiar Imagen</button>
                                 </div>
                             </div>
+                            
                         </form>
                     </div>
-                    <div class="col-md-4">
-                         <img src="{{ asset( 'imagenesPerfil/' . $user->imagenPerfil)}}" class="img-responsive" style="object-fit:cover; width:100%; max-width:150px;">
-                    </div>
+                    <style>
+                    .perfil{
+                        margin-left: -20px;
+                    }
+                    </style>
+                    
+
                 </div>
             </div>
             <div class="panel panel-default">
@@ -132,6 +146,9 @@
          </div>
     </div>
 </div>
+<style>
+
+</style>
 
     
              <script>
